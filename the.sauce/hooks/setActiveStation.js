264cc7027548bbc00ac08fbea7b4d178e0cb54ae
TumiKotane <kaiineoyoung@gmail.com>
2024-05-24 11:18:51 +0000

    channels added

---
+++ the.sauce/hooks/setActiveStation.js
@@ -0,0 +1,29 @@
+export const setActiveStation = (active) => {
+  if (active === "Ambient Renders") {
+    return "Ambient Renders";
+  } else if (active === "AnimeVibe") {
+    return "AnimeVibe";
+  } else if (active === "Astral Throb") {
+    return "Astral Throb";
+  } else if (active === "ChillHop Music") {
+    return "ChillHop Music";
+  } else if (active === "Chilled Cow") {
+    return "Chilled Cow";
+  } else if (active === "ChillAF") {
+    return "ChillAF";
+  } else if (active === "Ivy Records") {
+    return "Ivy Records";
+  } else if (active === "LofiBoy") {
+    return "LofiBoy";
+  } else if (active === "LofiGirl") {
+    return "LofiGirl";
+  } else if (active === "Nice Guys") {
+    return "Nice Guys";
+  } else if (active === "TOKYO TONES") {
+    return "TOKYO TONES";
+  } else if (active === "theJazz Cafe") {
+    return "theJazz Cafe";
+  } else if (active === "College Music") {
+    return "College Music";
+  }
+};
